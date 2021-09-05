c2c6f9905b6a1ecb05909824846aa6dc67823b9d
Malini299 <67904376+Malini299@users.noreply.github.com>
2021-09-05 08:08:54 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Head2 from './Head2';
+import Category from './Category';
+import Computer from './Computer';
+import Computer2 from './Computer2';
+import Computer3 from './Computer3';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Head2 />
+      <Category />
+      <Computer />
+      <Computer2 />
+      <Computer3 />
+      
+    </div>
+  );
+}
+
+export default App;
